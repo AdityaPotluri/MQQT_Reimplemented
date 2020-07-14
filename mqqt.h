c22c8fbb420c97269ecf7ed4fcbd765463f9ab3a
Aditya Potluri <adityapotluri1@gmail.com>
2020-07-13 23:20:32 -0400

    extra documentation for PACKETS

--- mqqt.h
+++ mqqt.h
@@ -17,6 +17,28 @@
 #define UNSUBACK_BYTE 0xB0
 #define PINGRESP_BYTE 0xD0
 
+/* 
+    CONNECT is the packet which requests a method to the server
+    CONNACK is the packet which acknowledges the connection to the server
+    PUBLISH "publishes" the message to the server
+    PUBAK acknowledges the publishing of the server
+    PUBREC responds to the server with something called QOS2 which is the quality of assurance that the packet has been delivered exactly once
+    PUBREL or publish release responds to PUBREC with the same packet identifier as PUBREC
+    PUBCOMP is the packet which responds to PUBREL and contains the same packet identifier to signal that it is being acknowledged
+    SUBSCRIBE is the packet which requests to create a subscription to a certain topic
+    SUBACK acknowledges the SUBSCRIBE packet
+    UNSUBSCRIBE unsubscribes from certain topics 
+    UNSUBACK acknowledges the unsubscription and as always with acknowledgements makes sure to have the same packet identifier
+    PINGREQ the pingreq can be used to indicate the client, server , and network connection is alive
+    PINGRESP responds to the PINGREQ
+    DISCONNECT is the final packet sent from client to the server to show that the client is disconnecting early
+
+
+
+
+
+
+*/
 enum packet_types{
     CONNECT     = 1,
     CONNACK     = 2,
@@ -53,7 +75,17 @@ struct mqqt_connect {
         unsigned char byte;
         struct {
             int reserved : 1;
-            unsigned clean_session:1;
-        }
+            unsigned clean_session : 1;
+            unsigned will : 1;
+            unsigned will_qos : 1;
+            unsigned will_retain : 1;
+            unsigned password : 1;
+            unsigned username : 1;
+        } bits;
+
+    };
+
+    struct {
+        unsigned short keepalive
     }
 }
